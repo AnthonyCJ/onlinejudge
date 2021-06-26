57716e8fcfe921fbe7a9a3f76a4f24df4c4ea1b2
AnthonyCJ <778018549@qq.com>
2021-06-26 17:09:22 +0800

    2021-06-26 XCJ new file Sword6PrintListFromTailToHead.java(implemented by stack).

--- sword/src/datastructure/linkedlist/Sword6PrintListFromTailToHead.java
+++ sword/src/datastructure/linkedlist/Sword6PrintListFromTailToHead.java
@@ -1,4 +1,30 @@
 package datastructure.linkedlist;
 
+import java.util.Stack;
+
+ class ListNode {
+      int val;
+     ListNode next;
+      ListNode(int x) { val = x; }
+ }
 public class Sword6PrintListFromTailToHead {
+
+    /**
+     * implemented by stack
+     * @param head head of the linkedlist
+     * @return an array of reversion
+     */
+    public int[] reversePrintStack(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            res[i++] = stack.pop();
+        }
+        return res;
+    }
 }
