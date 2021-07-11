62ca01247cc2768498b4494608d500f75bc87a08
AnthonyCJ <778018549@qq.com>
2021-07-11 22:23:56 +0800

    2021-07-11 XCJ modified file Sword6PrintListFromTailToHead.java (add Recursion and Iteration).

--- sword/src/datastructure/linkedlist/Sword6PrintListFromTailToHead.java
+++ sword/src/datastructure/linkedlist/Sword6PrintListFromTailToHead.java
@@ -1,5 +1,6 @@
 package datastructure.linkedlist;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
  class ListNode {
@@ -14,7 +15,7 @@ public class Sword6PrintListFromTailToHead {
      * @param head head of the linkedlist
      * @return an array of reversion
      */
-    public int[] reversePrintStack(ListNode head) {
+    public int[] reversePrint_Stack(ListNode head) {
         Stack<Integer> stack = new Stack<>();
         while (head != null) {
             stack.push(head.val);
@@ -24,6 +25,56 @@ public class Sword6PrintListFromTailToHead {
         int i = 0;
         while (!stack.isEmpty()) {
             res[i++] = stack.pop();
+        }
+        return res;
+    }
+
+    /**
+     * Recursion
+     * Time:O(n), Space:O(n)
+     * Version 1.0 2021-07-11 by XCJ
+     */
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    public int[] reversePrint_Recursion(ListNode head) {
+        recursion(head);
+        int[] res = new int[arrayList.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = arrayList.get(i);
+        }
+        return res;
+    }
+    void recursion(ListNode head) {
+        if (head == null) return;
+        recursion(head.next);
+        arrayList.add(head.val);
+    }
+
+    /**
+     * Iteration
+     * Time:O(n), Space:O(1)
+     * Version 2.0 2021-07-11 by XCJ
+     * @param head head of the list
+     * @return an array of reversion
+     */
+    public int[] reversePrint_Iteration(ListNode head) {
+        if (head == null) return new int[0];
+        ListNode oldHead = head, nextNode = head.next;
+        int size = 1;
+        while (oldHead.next != null) {
+            // cut
+            oldHead.next = nextNode.next;
+            // link
+            nextNode.next = head;
+            // update head
+            head = nextNode;
+            // iterate
+            nextNode = oldHead.next;
+            size++;
+        }
+        int[] res = new int[size];
+        for (int i = 0; i < size; i++ ) {
+            res[i] = head.val;
+            head = head.next;
         }
         return res;
     }
